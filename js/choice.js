a69cebeaca12f64323cf3f762ed0c7ff5b879cd5
Voldemort0702 <tommy.870702@gmail.com>
2023-04-10 16:01:17 -0400

    change background

--- js/choice.js
+++ js/choice.js
@@ -5,37 +5,7 @@ let main = document.getElementById('main');
 
 
 function changeBackground(){
-    let random = Math.floor(Math.random() * 6.9 );
-    console.log(random);
-    switch (random) {
-        case 0:
-            background.style.background = "url('image/background.png') no-repeat center center/cover";
-            break;
-        case 1:
-            background.style.background = "url('image/background2.png') no-repeat center center/cover";
-            break;
-        case 2:
-            background.style.background = "url('image/background3.png') no-repeat center center/cover";
-            break;
-        case 3:
-            background.style.background = "url('image/background4.png') no-repeat center cente/coverr";
-            break;
-        case 4:
-            background.style.background = "url('image/background5.png') no-repeat center center/cover";
-            break;
-        case 5:
-            background.style.background = "url('image/background6.png') no-repeat center center/cover";
-            break;
-        case 6:
-            background.style.background = "url('image/background7.png') no-repeat center center/cover";
-            break;
-        default:
-            background.style.background = "url('image/background7.png') no-repeat center center/cover";
-            break;
-    }
-
-    setTimeout(changeBackground, 10000);
-
+    background.style.background = "url('image/background.png') no-repeat center center/cover";
 }
 
 changeBackground();
